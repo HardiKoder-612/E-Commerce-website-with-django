ff48e71bafa86cce18b987b84a34120c85bc44b1
Hardik <nikku.sharmaklk@gmail.com>
2022-06-28 22:48:54 +0530

    Tracker modified

--- shop/views.py
+++ shop/views.py
@@ -44,16 +44,14 @@ def tracker(request):
     if request.method=="POST":
         orderId = request.POST.get('orderId', '')
         email= request.POST.get('email', '')
-        print(f"{orderId}\t\t{email}")
         try:
             order=Orders.objects.filter(order_id=orderId,email=email)
-            print(order)
             if len(order)>0:
                 update = OrderUpdate.objects.filter(order_id=orderId)
                 updates = []
                 for item in update:
                     updates.append({'text':item.update_desc,'time':item.timestamp})
-                    response=json.dumps(updates, default=str)
+                    response=json.dumps([updates, order[0].items_json], default=str)
                     return HttpResponse(response)
             else:
                 return HttpResponse("{}")
