281fc3041f2104ae681bbf1174faf922fcbaf3fc
Hardik <nikku.sharmaklk@gmail.com>
2022-07-01 13:00:38 +0530

    Blogs are also visible on the homepage vs blagapp but their designing is pending

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Blogpost
+from math import ceil
 # Create your views here.
 def index(request):
-    return render(request,"blog/index.html")
+    allPosts = []
+    titposts = Blogpost.objects.values('title', 'id')
+    titles = {item['title'] for item in titposts}
+    for title in titles:
+        post = Blogpost.objects.filter(title=title)
+        n = len(post)
+        nslides = n // 4 + ceil((n / 4) - (n // 4))
+        allPosts.append([post, range(1, nslides), nslides])
+    params = {'allposts': allPosts}
+    return render(request,"blog/index.html",params)
 
 def blogpost(request):
     return render(request,"blog/blogpost.html")
