b37d5e3e0cd75164f2eef58ee778b9fa47d4f148
Hardik <nikku.sharmaklk@gmail.com>
2022-07-02 00:58:20 +0530

    Everything for blogs is done

--- blog/views.py
+++ blog/views.py
@@ -2,19 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Blogpost
 from math import ceil
+
 # Create your views here.
 def index(request):
-    allPosts = []
-    titposts = Blogpost.objects.values('title', 'id')
-    titles = {item['title'] for item in titposts}
-    for title in titles:
-        post = Blogpost.objects.filter(title=title)
-        n = len(post)
-        nslides = n // 4 + ceil((n / 4) - (n // 4))
-        allPosts.append([post, range(1, nslides), nslides])
-    params = {'allposts': allPosts}
-    return render(request,"blog/index.html",params)
+    myposts=Blogpost.objects.all()
+    return render(request,"blog/index.html",{'myposts':myposts})
 
-def blogpost(request):
-    return render(request,"blog/blogpost.html")
+def blogpost(request,id):
+    post = Blogpost.objects.filter(id=id)[0]
+    return render(request,"blog/blogpost.html",{'post':post})
 
